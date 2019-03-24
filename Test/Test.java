bce8829186948f6e66650b67d03017b4413f9f5e
howardchristopher25 <howardchristopher25@gmail.com>
2019-03-24 17:11:13 +0000

    Master

---
+++ Test/Test.java
@@ -0,0 +1,42 @@
+package test;
+
+public class Test 
+{
+   public static void main(String[] args) 
+    {
+        boolean test;
+        test = true;
+        int array[] = new int[5];
+        
+        array[0] = 1;
+        array[1] = 2;
+        array[2] = 3;
+        array[3] = 4;
+        array[4] = 5;
+        
+        
+        for (int start : array)
+        {
+            if (start == 1)
+            {
+                System.out.println("1");
+            }
+            if (start == 2)
+            {
+                System.out.println("2");
+            }
+            if (start == 3)
+            {
+                System.out.println("3");
+            }
+            if (start == 4)
+            {
+                System.out.println("4");
+            }
+            if (start == 5)
+            {
+                System.out.println("5");
+            }
+        }
+    }
+}
